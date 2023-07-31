0b8a6245533126c5a718ccf56c92004f4b96c19c
Viktor Takacs <takacsv90@gmail.com>
2023-07-31 19:17:09 +0200

    CV

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import html from '../assets/lg/html.png'
-import css from '../assets/lg/css.png'
-import js from '../assets/lg/js.png'
-import Vite from '../assets/lg/Vite.png'
-import typescript from '../assets/lg/typescript.png'
-import react from '../assets/lg/react.png'
+import React from 'react';
+import html from '../assets/lg/html.png';
+import css from '../assets/lg/css.png';
+import js from '../assets/lg/js.png';
+import Vite from '../assets/lg/Vite.png';
+import typescript from '../assets/lg/typescript.png';
+import react from '../assets/lg/react.png';
 
 const Languages = () => {
-    const frontendLanguages = [
-        {
-          name: 'HTML',
-          image: html,
-        },
-        {
-          name: 'CSS',
-          image: css,
-        },
-        {
-          name: 'JavaScript',
-          image: js,
-        },
-        {
-          name: 'Typescript',
-          image: typescript,
-        },
-      ];
+  const frontendLanguages = [
+    {
+      name: 'HTML',
+      image: html,
+    },
+    {
+      name: 'CSS',
+      image: css,
+    },
+    {
+      name: 'JavaScript',
+      image: js,
+    },
+    {
+      name: 'TypeScript',
+      image: typescript,
+    },
+  ];
 
-    const frameworks = [
-        {
-          name: 'React',
-          image: react,
-        },
-        {
-          name: 'Vite',
-          image: Vite,
-        },
-      ]; 
+  const frameworks = [
+    {
+      name: 'React',
+      image: react,
+    },
+    {
+      name: 'Vite',
+      image: Vite,
+    },
+  ];
 
   return (
     <div id='languages' className="container mx-auto mt-10 py-10 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
@@ -46,28 +46,27 @@ const Languages = () => {
             <img
               src={language.image}
               alt={language.name}
-              className="w-full h-40 object-cover mb-4"
+              className="w-full h-40 sm:h-40 object-cover mb-4"
             />
             <p className="text-lg font-semibold">{language.name}</p>
           </div>
         ))}
       </div>
       <h1 className='text-4xl font-bold mb-10 mt-16 text-center'>Frameworks</h1>
-      <div className='gap-4 flex justify-center items-center'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-2 justify-items-center'>
         {frameworks.map((language, index) => (
           <div key={index} className="bg-white rounded shadow-md p-10">
             <img
               src={language.image}
               alt={language.name}
-              className="w-full h-40 object-cover mb-4"
+              className="w-full h-40 sm:h-40 object-cover mb-4"
             />
             <p className="text-lg font-semibold">{language.name}</p>
           </div>
         ))}
       </div>
     </div>
- 
-  )
-}
+  );
+};
 
-export default Languages
+export default Languages;
