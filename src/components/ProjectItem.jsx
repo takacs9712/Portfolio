710446289c2bb07e33d6bdf4339e7b1cf14eb60c
Viktor Takacs <takacsv90@gmail.com>
2023-07-31 23:08:35 +0200

    add

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -13,7 +13,7 @@ const ProjectItem = ({img, title}) => {
       <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
         {title}
       </h3>
-      <p className='pb-4 pt-2 text-white text-center'>React JS</p>
+      <p className='pb-4 pt-2 text-white text-center'>React</p>
       <a href="/">
         <p
           className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg'
