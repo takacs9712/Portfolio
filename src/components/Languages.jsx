b7eea5f19010c111def7c9414649072ced26710d
Viktor Takacs <takacsv90@gmail.com>
2023-08-02 13:24:46 +0200

    Ajudsted the sizes

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -40,28 +40,29 @@ const Languages = () => {
   return (
     <div id='languages' className="container mx-auto mt-10 py-10 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
       <h1 className="text-4xl font-bold mb-10 text-center">Languages</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-2 justify-items-center'>
         {frontendLanguages.map((language, index) => (
-          <div key={index} className="bg-white rounded shadow-md p-10">
+          <div key={index} className="bg-white rounded shadow-md h-64 p-6 sm:p-10 flex flex-col justify-between">
             <img
               src={language.image}
               alt={language.name}
-              className="w-full h-40 sm:h-40 object-cover mb-4"
+              className="w-full h-32 sm:h-32 md:h-40 object-cover mb-4"
             />
-            <p className="text-lg font-semibold">{language.name}</p>
+            <p className="text-lg font-semibold text-center">{language.name}</p>
           </div>
         ))}
       </div>
+
       <h1 className='text-4xl font-bold mb-10 mt-16 text-center'>Frameworks</h1>
       <div className='grid grid-cols-1 sm:grid-cols-2 gap-2 justify-items-center'>
         {frameworks.map((language, index) => (
-          <div key={index} className="bg-white rounded shadow-md p-10">
+          <div key={index} className="bg-white rounded shadow-md h-64 p-6 sm:p-10 flex flex-col justify-between">
             <img
               src={language.image}
               alt={language.name}
-              className="w-full h-40 sm:h-40 object-cover mb-4"
+              className="w-full h-32 sm:h-32 md:h-40 object-cover mb-4"
             />
-            <p className="text-lg font-semibold">{language.name}</p>
+            <p className="text-lg font-semibold text-center">{language.name}</p>
           </div>
         ))}
       </div>
