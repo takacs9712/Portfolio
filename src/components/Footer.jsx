ee3a2cd39ee8fbdc85b8eeb565d2c1c68a8b0ac5
Viktor Takacs <takacsv90@gmail.com>
2023-08-14 16:54:55 +0200

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto text-center">
+      <div className="container mx-auto text-center flex justify-center">
         <p>© {new Date().getFullYear()} Takács Viktor. All rights reserved.</p>
+        <p className='ml-10'>This website was created using <strong>React + Vite</strong></p>
       </div>
     </footer>
   )
