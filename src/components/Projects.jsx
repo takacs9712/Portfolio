9a5439d8cdeb116660986e1c8e2acb28fe48d1d2
Viktor Takacs <takacsv90@gmail.com>
2023-09-29 21:07:20 +0200

    Update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ProjectItem from "./ProjectItem";
-import restaurant from "../assets/restaurant.png";
+import Restaurant from "../assets/Restaurant.png";
 import placeholder from "../assets/placeholder.png";
 import Syncee1 from "../assets/Syncee1.png";
 import animal from "../assets/animal.png";
@@ -39,7 +39,7 @@ const Projects = () => {
           <ProjectItem img={uploader} title="Image Upload/Download" />
         </Link>
         <Link to="/Portfolio/FoodOrder">
-          <ProjectItem img={restaurant} title="restaurant" />
+          <ProjectItem img={Restaurant} title="restaurant" />
         </Link>
         <ProjectItem img={placeholder} title="In Progress..." />
         <ProjectItem img={placeholder} title="In Progress..." />
